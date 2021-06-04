fdea9503b9941b82f572c3b21eaa6baa2b887de3
BeZJIuKiy <matveenkov.sasha@gmail.com>
2021-06-04 14:18:21 +0300

    maket is worked

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,12 @@
 import React from "react"
 import {Header} from "./Header";
 import {MainDrawer} from "./MainDrawer";
-// import {NewTabs_02} from "./Prediction/Tabs_02";
 
 export const Main = () => {
     return (
         <div>
             <Header/>
             <MainDrawer/>
-            {/*<NewTabs_02 />*/}
         </div>
     )
 }
