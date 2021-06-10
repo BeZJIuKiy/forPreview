ed359c64b1d25bcba3d22bcdbbd46872ca600115
BeZJIuKiy <matveenkov.sasha@gmail.com>
2021-06-10 10:50:31 +0300

    added notification

--- src/components/Main/Notification.js
+++ src/components/Main/Notification.js
@@ -1,58 +1,3 @@
-// import React from 'react';
-// import Button from '@material-ui/core/Button';
-// import Snackbar from '@material-ui/core/Snackbar';
-// import MuiAlert from '@material-ui/lab/Alert';
-// import { makeStyles } from '@material-ui/core/styles';
-//
-// function Alert(props) {
-//     return <MuiAlert elevation={6} variant="filled" {...props} />;
-// }
-//
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         width: '100%',
-//         '& > * + *': {
-//             marginTop: theme.spacing(2),
-//         },
-//     },
-// }));
-//
-// export const Notifications = () => {
-//     const classes = useStyles();
-//     const [open, setOpen] = React.useState(false);
-//
-//     const handleClick = () => {
-//         setOpen(true);
-//     };
-//
-//     const handleClose = (event, reason) => {
-//         if (reason === 'clickaway') {
-//             return;
-//         }
-//
-//         setOpen(false);
-//     };
-//
-//     return (
-//         <div className={classes.root}>
-//             <Button variant="outlined" onClick={handleClick}>
-//                 Open success snackbar
-//             </Button>
-//             {/*<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>*/}
-//             <Snackbar open={open} autoHideDuration={1000} onClose={handleClose}>
-//                 <Alert onClose={handleClose} severity="success">
-//                     This is a success message!
-//                 </Alert>
-//             </Snackbar>
-//             <Alert severity="error">This is an error message!</Alert>
-//             <Alert severity="warning">This is a warning message!</Alert>
-//             <Alert severity="info">This is an information message!</Alert>
-//             <Alert severity="success">This is a success message!</Alert>
-//         </div>
-//     );
-// }
-
-
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -61,108 +6,130 @@ import CloseIcon from '@material-ui/icons/Close';
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    root: {
-        backgroundColor: "#f44336"+'!important',
-        fontSize: 16,
-        color: "gold"+'!important',
-    }
+	root: {
+		backgroundColor: "#f44336" + '!important',
+		fontSize: 16,
+		padding: "0 60px 10px"
+		// color: "white"+'!important',
+	},
+
+	message: {
+		color: "white",
+		fontWeight: 700,
+		fontSize: 16,
+
+		position: "relative",
+	},
+
+	message__title: {
+		fontSize: 18,
+		textAlign: "center",
+	},
+
+	icon__pos: {
+		position: "absolute",
+		top: 0,
+		right: 0,
+		zIndex: 1,
+
+		display: "block",
+	}
 });
 
-const styles = {
-    root: {
-        background: 'red'
-    }
-};
-
 export const Notifications = () => {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
 
-    const handleClick = () => {
-        setOpen(true);
-    };
+	const handleClick = () => {
+		setOpen(true);
+	};
 
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
 
-        setOpen(false);
-    };
+		setOpen(false);
+	};
 
-    return (
-        <div>
-            <Button onClick={handleClick}>Open simple snackbar</Button>
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                }}
-                ContentProps={{
-                    classes: {
-                        root: classes.root
-                    }
-                }}
-                open={open}
-                // autoHideDuration={60000}
-                autoHideDuration={null}
-                onClose={handleClose}
-                // message="Note archived"
+	return (
+		<div>
+			<Button onClick={handleClick}>Open simple snackbar</Button>
+			<Snackbar
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'right',
+				}}
+				ContentProps={{
+					classes: {
+						root: classes.root
+					}
+				}}
+				open={open}
+				// autoHideDuration={60000}
+				autoHideDuration={null}
+				onClose={handleClose}
 
-                message={
-                    <span id="message-id">
-                        <div>
-                            <h3 style={{textAlign: "center"}}>Title</h3>
-                            <div>Hi there! Some message.</div>
+				message={
+					<span id="message-id">
+                        <div className={classes.message}>
+                            <h3 className={classes.message__title}>
+                                {/*{"Ship 01 / Engine 02"}*/}
+	                            {"АСНКВ"}
+                            </h3>
+	                        {/*<div className={classes.message}>{"Attention! Exceeding the emission of ammonia!"}</div>*/}
+	                        <div>{"Внимание! Превышение по выбросу аммиака!"}</div>
                         </div>
+
+						<div style={{
+							display: "flex",
+							justifyContent: "space-between",
+							color: "white",
+							paddingTop: 10,
+						}}>
+							{/*<div>Time: 12:04</div>*/}
+							{/*<div>Date: 31.05.2019</div>*/}
+
+							<div>Время: 12:04</div>
+							<div>Дата: 31.05.2019</div>
+						</div>
+
+	                    <div style={{display: "flex", justifyContent: "space-around"}}>
+		                    <Button color="primary" size="large" onClick={handleClose}
+		                            style={{
+			                            width: "50%",
+			                            fontSize: 14,
+			                            background: "#ddd",
+			                            marginTop: 20,
+			                            color: "#333"
+		                            }}
+		                    >
+		                        {/*recommendations*/}
+			                    рекомендации
+		                    </Button>
+	                    </div>
+
+						<div className={classes.icon__pos}>
+							<IconButton size="large" aria-label="close" color="inherit" onClick={handleClose}>
+				 			    <CloseIcon fontSize="large" style={{color: "#333"}}/>
+					        </IconButton>
+						</div>
+
                     </span>
-                }
+				}
 
 
-                action={
-                    <React.Fragment>
-                        <Button color="secondary" size="small" onClick={handleClose}>
-                            UNDO
-                        </Button>
-                        <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
-                            <CloseIcon fontSize="small"/>
-                        </IconButton>
-                    </React.Fragment>
-                }
-            />
-        </div>
-    );
+				// action={
+				// 	<React.Fragment>
+				// 		{/*<Button color="secondary" size="small" onClick={handleClose}>*/}
+				// 		{/*    UNDO*/}
+				// 		{/*</Button>*/}
+				// 		<IconButton size="large" aria-label="close" color="inherit" onClick={handleClose}>
+				// 			<CloseIcon fontSize="large" style={{color: "#333"}}/>
+				// 		</IconButton>
+				// 	</React.Fragment>
+				// }
+			/>
+		</div>
+	);
 }
-
-
-// import React from 'react';
-// import Button from '@material-ui/core/Button';
-// import { SnackbarProvider, useSnackbar } from 'notistack';
-//
-// function MyApp() {
-//     const { enqueueSnackbar } = useSnackbar();
-//
-//     const handleClick = () => {
-//         enqueueSnackbar('I love snacks.');
-//     };
-//
-//     const handleClickVariant = (variant) => () => {
-//         // variant could be success, error, warning, info, or default
-//         enqueueSnackbar('This is a success message!', { variant });
-//     };
-//
-//     return (
-//         <React.Fragment>
-//             <Button onClick={handleClick}>Show snackbar</Button>
-//             <Button onClick={handleClickVariant('success')}>Show success snackbar</Button>
-//         </React.Fragment>
-//     );
-// }
-//
-// export const Notifications = () => {
-//     return (
-//         <SnackbarProvider maxSnack={3}>
-//             <MyApp />
-//         </SnackbarProvider>
-//     );
-// }
